54e39435ff9ee6f4afe8e71e43bcd20e092b66d6
Anton Pomogaev <antonpomogaev@gmail.com>
2022-04-26 22:41:17 +0300

    feat: add solution for stack problem

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.storage = [];
+  }
+
+  push(element) {
+    this.storage.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.storage.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const lastIndex = this.storage.length - 1;
+    return this.storage[lastIndex];
   }
 }
 
